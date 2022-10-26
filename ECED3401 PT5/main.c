cc071ef32c70d8828e1f9a12bb6d032a4822c51c
amellalalex <github@alexamellal.com>
2022-10-26 09:03:52 -0300

    now taking map size as program args

--- ECED3401 PT5/main.c
+++ ECED3401 PT5/main.c
@@ -3,9 +3,19 @@
 
 // Standard C libraries
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	Map m = (Map){ .width = 5, .height = 5 };
+int main(int argc, char *argv[]) {
+	// Check argc 
+	if(argc != 3) {
+		printf("[!] Invalid number of args. Try ./a.out <width> <height>\n");
+		return -1;
+	}
+
+	int width = atoi(argv[1]);
+	int height = atoi(argv[2]);
+
+	Map m = (Map){ .width = width, .height = height };
 
 	render_map(m);
 
