888b9a7fd3fff0ac77ab55b561a2cc422bd0653e
Alex <github@alexamellal.com>
2022-10-30 16:09:13 -0300

    getting main ready for drive around demo

--- ECED3401 PT5/main.c
+++ ECED3401 PT5/main.c
@@ -26,40 +26,27 @@ int main(int argc, char *argv[]) {
 		.console = GetStdHandle(STD_OUTPUT_HANDLE),
 	};
 
-	/*Taxi t = (Taxi){
-		.taxi_pos = {.x = 1, .y = 2, .cx = 3, .cy = 0},
-	};*/
-
-	/*for (;;) {
-		Pos randp = get_random_pos(m);
-		printf("get_random_pos(m) = (Pos) { .x = %d, .y = %d, .cx = %d, .cy = %d }\n", randp.x, randp.y, randp.cx, randp.cy);
-		Sleep(500);
-	}*/
-
-	for (;;) {
-		//update_taxis();
-
-		Taxi t = (Taxi){
-			.orientation = EAST,
-			.taxi_pos = get_random_pos(m),
-			//.taxi_pos = (Pos){.x = 3, .y = 4, .cx = 0, .cy = 0}
+	Taxi taxis[MAX_NUM_TAXIS];
+	for (int x = 0; x < MAX_NUM_TAXIS; x++) {
+		taxis[x] = (Taxi){
+			.state = TAXI_STATE_DRIVING,
+			.origin = get_random_pos(m),
+			.destination = get_random_pos(m),
 		};
+		taxis[x].taxi_pos = taxis[x].origin;
+	}
 
-		// Render 
-		render_map(m);
+	int driving_taxis = MAX_NUM_TAXIS;
+	while (driving_taxis > 0) {
 
-		printf("get_random_pos(m) = (Pos) { .x = %d, .y = %d, .cx = %d, .cy = %d }\n", t.taxi_pos.x, t.taxi_pos.y, t.taxi_pos.cx, t.taxi_pos.cy);
 
-		render_taxi(m, t);
 
-		// Update
-		/*t.taxi_pos = offset_cpos(t.taxi_pos, 1, 0);
-		t.orientation++;
-		if (t.orientation > WEST) {
-			t.orientation = NORTH;
-		}*/
-
-		Sleep(500);
+		driving_taxis = 0;
+		for (int x = 0; x < MAX_NUM_TAXIS; x++) {
+			if (taxis[x].state == TAXI_STATE_DRIVING) {
+				driving_taxis++;
+			}
+		}
 	}
 
 	getchar();
