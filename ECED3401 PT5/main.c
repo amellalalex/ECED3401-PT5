bc832006ac709a1516f3f6d9a5c3dad44e36e972
Alex <github@alexamellal.com>
2022-10-30 15:13:45 -0300

    main used for position testing

--- ECED3401 PT5/main.c
+++ ECED3401 PT5/main.c
@@ -26,29 +26,38 @@ int main(int argc, char *argv[]) {
 		.console = GetStdHandle(STD_OUTPUT_HANDLE),
 	};
 
-	Taxi t = (Taxi){
+	/*Taxi t = (Taxi){
 		.taxi_pos = {.x = 1, .y = 2, .cx = 3, .cy = 0},
-	};
+	};*/
 
-	for (;;) {
+	/*for (;;) {
 		Pos randp = get_random_pos(m);
 		printf("get_random_pos(m) = (Pos) { .x = %d, .y = %d, .cx = %d, .cy = %d }\n", randp.x, randp.y, randp.cx, randp.cy);
 		Sleep(500);
-	}
+	}*/
 
 	for (;;) {
 		//update_taxis();
 
+		Taxi t = (Taxi){
+			.orientation = EAST,
+			.taxi_pos = get_random_pos(m),
+			//.taxi_pos = (Pos){.x = 3, .y = 4, .cx = 0, .cy = 0}
+		};
+
 		// Render 
 		render_map(m);
+
+		printf("get_random_pos(m) = (Pos) { .x = %d, .y = %d, .cx = %d, .cy = %d }\n", t.taxi_pos.x, t.taxi_pos.y, t.taxi_pos.cx, t.taxi_pos.cy);
+
 		render_taxi(m, t);
 
 		// Update
-		t.taxi_pos = offset_cpos(t.taxi_pos, 1, 0);
+		/*t.taxi_pos = offset_cpos(t.taxi_pos, 1, 0);
 		t.orientation++;
 		if (t.orientation > WEST) {
 			t.orientation = NORTH;
-		}
+		}*/
 
 		Sleep(500);
 	}
