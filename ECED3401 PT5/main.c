090c35fd241694d713e641fadddfa48f68f071db
Alex <github@alexamellal.com>
2022-10-31 13:28:37 -0300

    worked more on phony navigation

--- ECED3401 PT5/main.c
+++ ECED3401 PT5/main.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[]) {
 		.console = GetStdHandle(STD_OUTPUT_HANDLE),
 	};
 
+	// Init taxis
 	Taxi taxis[MAX_NUM_TAXIS];
 	for (int x = 0; x < MAX_NUM_TAXIS; x++) {
 		taxis[x] = (Taxi){
@@ -38,15 +39,36 @@ int main(int argc, char *argv[]) {
 
 	int driving_taxis = MAX_NUM_TAXIS;
 	while (driving_taxis > 0) {
+		// Re-render map
+		render_map(m);
+	
+		char* heading_msg;
+		printf("\n\n");
+		for (int x = 0; x < MAX_NUM_TAXIS; x++) {
+			char* heading_msg = get_heading_msg(m, taxis[x].taxi_pos, taxis[x].destination);
+			printf(
+				"taxis[%d] => pos: {(%d, %d), (%d, %d)}, dest: {(%d, %d), (%d, %d)}, HEADING: %s\n", x,
+				taxis[x].taxi_pos.x, taxis[x].taxi_pos.y, taxis[x].taxi_pos.cx, taxis[x].taxi_pos.cy,
+				taxis[x].destination.x, taxis[x].destination.y, taxis[x].destination.cx, taxis[x].destination.cy,
+				heading_msg
+			);
+		}
 
+		// Update taxi pos & render
+		for (int x = 0; x < MAX_NUM_TAXIS; x++) {
+			taxis[x].taxi_pos = next_pos(m, taxis[x]);
+			render_taxi(m, taxis[x]);
+		}
 
-
+		// Count driving taxis
 		driving_taxis = 0;
 		for (int x = 0; x < MAX_NUM_TAXIS; x++) {
 			if (taxis[x].state == TAXI_STATE_DRIVING) {
 				driving_taxis++;
 			}
 		}
+
+		Sleep(500);
 	}
 
 	getchar();
--- ECED3401 PT5/taxi.h
+++ ECED3401 PT5/taxi.h
@@ -2,7 +2,7 @@
 #include "structs.h"
 
 //function to scan taxi
-void update_taxi(Taxi T);
+void update_taxi(Taxi* T);
 
 
 void navigate_taxi(Taxi T, Pos final_pos);
